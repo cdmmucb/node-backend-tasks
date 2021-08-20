5bb2e97942fa2febb5913875b178788ece9fe4c3
cdmmucb <carlos.mendizabal@ucb.edu.bo>
2021-08-20 15:29:31 -0400

    calls

--- index.js
+++ index.js
@@ -10,19 +10,75 @@ var app = express();
 
 var tasks = []
 
+var surrogateKey = 1;
+
+
 app.get("/", (req, res, next) => {
     res.json("{ 'message': 'Tasks server online'}");
 });
 
 app.post("/tasks", jsonParser, (req, res, next) => {
-    req.body.id = tasks.length + 1;
+    req.body.id = surrogateKey++;
+    req.body.state = "pending";
     tasks.push(req.body);
-    res.send("OK");
+    res.send("{'message':'Add successful'}");
 });
+
 
 app.get("/tasks", (req, res, next) => {
     res.json(tasks);
 });
+
+
+app.get('/tasks/:id', (req, res,next) =>{
+    var result = search(req.params.id, tasks);
+    res.json(result);
+});
+
+function search(id, array){
+    for (var i=0; i < array.length; i++) {
+        if (array[i].id == id) {
+            return array[i];
+        }
+    }
+}
+
+function searchPosition(id, array){
+    for (var i=0; i < array.length; i++) {
+        if (array[i].id == id) {
+            return i;
+        }
+    }
+}
+
+app.put("/tasks/:id", jsonParser, (req, res, next) => {
+    const state = req.query.state;
+    
+    var task = search(req.params.id,tasks);
+    
+    if (state) {
+ 
+        req.body.title = task.title;
+        req.body.detail = task.detail;
+        req.body.id = task.id;
+        req.body.state = state;
+        
+    } else {
+        req.body.id = task.id;
+                req.body.state = task.state;
+                
+    }
+    tasks.splice(searchPosition(task, tasks), 1, req.body);
+        res.send("{'message':'Modification successful'}");
+});
+
+
+app.delete('/tasks/:id', function(req, res) {
+    tasks.splice(searchPosition(req.params.id,tasks), 1);
+    res.send("{'message':'Deletion successful'}");
+});
+
+
 
 app.listen(3000, () => {
     console.log("Servidor HTTP funcionando");
